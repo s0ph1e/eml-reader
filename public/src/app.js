4e154eea5645ee9646570a50db3058e2de7e2ef6
PaulchenPlump <info@paulchenplump.de>
2019-09-26 17:58:45 +0200

    different improvements (#7)
    * Update uncritical dependencies
    
    * Update webpack.config.js
    
    * Update package.json
    
    * Create sample file for german language
    
    * dynamic loading of the language file
    
    * replace fixed values with language variables
    
    * line breaks and other font for text-mails
    
    * parse eml (rfc822) files only
    
    otherwise sends empty Mail with error-header
    
    * error handling for non eml-files
    
    * show eml_error
    
    * change language-strings to remove hardcoded whitespaces
    
    * change language-strings
    
    * forget closing bracket
    
    * Update de.json

--- public/src/app.js
+++ public/src/app.js
@@ -13,6 +13,11 @@ $(function() {
 	var headersContent = '.eml-details__content-headers';
 	var attachmentsContent = '.eml-details__content-attachments';
 
+	$('button.open-file').html(__('Choose file'));
+        $('.drag-and-drop-label').html(__('or drag and drop here'));
+        $('#headingAttachments h4 a').html(__('Attachments'));
+        $('#headingHeaders h4 a').html(__('Headers'));
+
 	$(document).on('click', openFileDialogBtn, function(e) {
 		e.preventDefault();
 		$(fileInput).trigger('click');
@@ -66,7 +71,7 @@ $(function() {
 					$(viewPage).fadeIn();
 				});
 				var doc = $(iframe)[0].contentWindow.document;
-				$(doc).find('body').html(data.html || data.text);
+				$(doc).find('body').html(data.html || '<div style="font-family: courier;">'+data.text.replace(/\r?\n/g, '<br />')+'</div>');
 				showDetails({headers: data.headers, attachments: data.attachments})
 			}
 		});
@@ -76,27 +81,35 @@ $(function() {
 		var headers = data.headers;
 		var attachments = data.attachments;
 
-		var headersToDisplay = ['subject', 'from', 'to', 'cc', 'date'];
+		var headersToDisplay = {'subject':__('subject: '), 'from':__('from: '), 'to':__('to: '), 'cc':__('cc: '), 'date':__('date: '),
+					'eml_error':__('This is not a valid eml-file. Please upload a new one!')};
 		var headersElements = [];
-		headersToDisplay.forEach(function (headerKey) {
+		for(var headerKey in headersToDisplay) {
 			if (headers[headerKey]) {
-				var el = document.createElement('div');
-				var key = document.createElement('b');
-				key.appendChild(document.createTextNode(headerKey + ': '));
-				var value = document.createElement('span');
-				value.appendChild(document.createTextNode( headers[headerKey]));
+                               	if (headerKey == 'eml_error') {
+                                	var el = document.createElement('div');
+                                       	el.setAttribute('id', 'eml_error_header');
+                                       	el.appendChild(document.createTextNode(headersToDisplay[headerKey]));
+                               	} else {
+                                       	var el = document.createElement('div');
+                                       	var key = document.createElement('b');
+                                       	key.appendChild(document.createTextNode(headersToDisplay[headerKey]));
+                                       	var value = document.createElement('span');
+                                       	value.appendChild(document.createTextNode(headers[headerKey]));
 
-				el.append(key, value);
+                                       	el.append(key, value);
+                               	}
+
 				headersElements.push(el);
 			}
-		});
-        $(headersContent).html('').append(headersElements);
+		};
+        	$(headersContent).html('').append(headersElements);
 
 		if (attachments && attachments.length > 0) {
 			var attachmentsElements = [];
 			attachments.forEach(function (attachment) {
 				var el = document.createElement('button');
-				el.appendChild(document.createTextNode('download ' + attachment.fileName));
+				el.appendChild(document.createTextNode(__('download ') + attachment.fileName));
 				el.addEventListener( 'click', function() {
 					downloadFile(attachment.content.data, attachment.fileName);
 				});
@@ -104,7 +117,7 @@ $(function() {
 			});
 			$(attachmentsContent).html('').append(attachmentsElements);
 		} else {
-			$(attachmentsContent).html('No attachments');
+			$(attachmentsContent).html(__('No attachments'));
 		}
 
 		$(details).show();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require('path');
-
-module.exports = {
-    entry: './public/src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'public/dist'),
-        filename: 'app.js'
-    },
-    resolve: {
-        alias: {
-            jquery: "jquery/src/jquery"
-        }
-    }
+var I18nPlugin = require('i18n-webpack-plugin');
+var languages = {
+    en: null,
+    de: require(path.resolve(__dirname, 'locales/de.json'))
 };
+
+module.exports = Object.keys(languages).map(function(language) {
+    return {
+        entry: './public/src/app.js',
+        output: {
+            path: path.resolve(__dirname, 'public/dist'),
+            filename: 'app-'+language+'.js'
+        },
+        resolve: {
+            alias: {
+                jquery: "jquery/src/jquery"
+            }
+        },
+        plugins: [new I18nPlugin(languages[language])]
+    };
+});
